ba29d110c35f819a4e5655e0b3f82394006a40be
Carl Boettiger <cboettig@gmail.com>
2017-06-20 23:38:27 +0000

    working on public data examples

--- R/movebank.R
+++ R/movebank.R
@@ -9,6 +9,7 @@ base <- "https://www.movebank.org/movebank/service/direct-read"
 #' @param ... additional fields to filter on as named list, depends on table selecte
 #' @param user username. Will be read from Sys.getenv("MOVEBANK_USER") if not supplied, register at https://www.movebank.org/
 #' @param password user password. Will be read from Sys.getenv("MOVEBANK_PW") if not supplied, register at https://www.movebank.org/
+#' @param guess_types logical. If FALSE, will read all cols as chracters (can facilitate row binding data.frames). Default TRUE (default read_csv behavior).
 #' @return Returns the requested table (read by read_csv), possibly filtered by additional search fields
 #' @details Acceptable table names are: deployment, event, individual, sensor, study, study_attribute, tag, tag_type, taxon
 
@@ -25,6 +26,7 @@ movebank <- function(table = NULL,
                      ...,
                      user = Sys.getenv("MOVEBANK_USER"),
                      password = Sys.getenv("MOVEBANK_PW"),
+                     guess_types = TRUE,
                      encoding = "UTF-8"){
 
 ## As listed by https://www.movebank.org/movebank/service/direct-read?attributes
@@ -37,6 +39,11 @@ movebank <- function(table = NULL,
     query <- list(entity_type = table, ...)
   }
 
+  if(guess_types)
+    col_types <- NULL
+  else
+    col_types <- readr::cols(.default = "c")
+
   ## write methods to use select & filter verbs, taxon %>% select(study_id) %>% filter(study) %>% call_api()
 
   ## FIXME adjust so that we can take lists of studies, or filters
@@ -46,7 +53,7 @@ movebank <- function(table = NULL,
   auth <- httr::authenticate(user, password)
   resp <- httr::GET(base, query = query, auth)
   httr::warn_for_status(resp)
-  httr::content(resp, as="parsed", encoding = encoding)
+  httr::content(resp, as="parsed", encoding = encoding, col_types = col_types)
 
 }
 
